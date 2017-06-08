d5b00b0f5c6fa811a9db7220c0435c1cf4bfda33
Dawid <dawid.aksamski@gmail.com>
2017-06-08 20:08:31 +0200

    Removed app

--- WebApp/account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    # post views
-    url(r'^login/$', views.user_login, name='login'),
-    url(r'^register/$', views.register, name='register'),
-]
